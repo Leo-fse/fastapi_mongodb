5f349cc89b1e57f3c44a1134733bcda230edb68e
tmk <morimoto.ke@gmail.com>
2021-12-29 10:02:06 +0900

    ✨ MongoDBの認証例を2パターン追記

--- config/db.py
+++ config/db.py
@@ -1,3 +1,29 @@
-import pymongo
+# Authentication Examples
+# https://pymongo.readthedocs.io/en/stable/examples/authentication.html
 
-conn = pymongo.MongoClient(host="localhost", port=27017)
+import urllib.parse
+
+from pymongo import MongoClient
+
+host = "localhost"
+port = 27017
+username = urllib.parse.quote_plus("username")
+password = urllib.parse.quote("password")
+authSource = "admin"
+authMechanism = "SCRAM-SHA-256"
+
+conn = MongoClient(
+    host=host,
+    port=port,
+    usename=username,
+    password=password,
+    authSource=authSource,
+    authMechamism="SCRAM-SHA-256",
+)
+
+uri = (
+    f"mongodb://{username}:{password}@{host}/"
+    f"?authSource={authSource}&authMechanism={authMechanism}"
+)
+
+conn2 = MongoClient(uri)
